04be7790ef0815a6e4982044d5e297bbb78540a2
Ivaylo Ivanov <ivaylo07082000@gmail.com>
2025-06-01 17:11:38 +0300

    Added index.html with functions to easily test API endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import itemsRouter from "./routes/itemsRouter";
-import { errorHandler } from "./middleware/error";
+import itemsRouter from "../routes/itemsRouter";
+import { errorHandler } from "../middleware/error";
+import path from "path";
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/", itemsRouter);
 app.use(errorHandler);
